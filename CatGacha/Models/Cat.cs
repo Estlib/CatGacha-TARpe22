f70937a0665e09028b9ccf4c09368211b3298a3b
Jüri Vaitmaa <jyri.vaitmaa@tthk.ee>
2024-08-28 15:03:25 +0300

    added models for user and cat, employed ENUM and booleans

---
+++ CatGacha/Models/Cat.cs
@@ -0,0 +1,17 @@
+﻿namespace CatGacha.Models
+{
+    public enum Zodiac
+    {
+        A, B, C, D, E, F, G, H, I, J, K, L
+    }
+    public class Cat
+    {
+        public int CatID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Zodiac Zodiac { get; set; }
+        public string Snack { get; set; }
+        public string Personality { get; set; }
+        public ICollection<CatOwnership> CatOwnerships { get; set; }
+    }
+}
---
+++ CatGacha/Models/User.cs
@@ -0,0 +1,20 @@
+﻿namespace CatGacha.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string Password { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public DateTime SignupAt { get; set; }
+        public bool IsDisabled { get; set; }
+        public string DisableReason { get; set; }
+
+        public ICollection<CatOwnership> CatsOwned { get; set; }
+
+        //
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+    }
+}
