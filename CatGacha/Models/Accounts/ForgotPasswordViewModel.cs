27b9dccec9dbd897feba38fa5e81d933692450ce
Estlib <75094683+Estlib@users.noreply.github.com>
2024-09-11 15:41:08 +0300

    =[IMPORTANT NOTE]= description of two previous commits were incorrect. the last commit added changepassword view and models. the one before that did not add what it claimed, but instead, both commits were commited under last commit. =[CURRENT COMMIT]= this commit adds forgotpassword view and model

--- CatGacha/Models/Accounts/ForgotPasswordViewModel.cs
+++ CatGacha/Models/Accounts/ForgotPasswordViewModel.cs
@@ -1,6 +1,11 @@
-﻿namespace CatGacha.Models.Accounts
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CatGacha.Models.Accounts
 {
     public class ForgotPasswordViewModel
     {
+        [Required]
+        [EmailAddress]
+        public string RecoveryEmail { get; set; }
     }
 }
