82d4db37db5e3b3e773b8e6ed2f08b6e011cd9d5
Estlib <75094683+Estlib@users.noreply.github.com>
2024-09-11 15:27:04 +0300

    added data into the AddPasswordViewModel, and its corresponding views

--- CatGacha/Models/Accounts/AddPasswordViewModel.cs
+++ CatGacha/Models/Accounts/AddPasswordViewModel.cs
@@ -1,6 +1,16 @@
-﻿namespace CatGacha.Models.Accounts
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CatGacha.Models.Accounts
 {
     public class AddPasswordViewModel
     {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Uus Salasõna")]
+        public string NewPassword { get; set; }
+        [Required]
+        [DataType (DataType.Password)]
+        [Compare("NewPassword", ErrorMessage = "Salasõnad ei ühti, palun kontrolli et kirjutasid mõlemad samamoodi.")]
+        public string ConfirmPassword { get; set;}
     }
 }
--- CatGacha/Models/Accounts/ChangePasswordViewModel.cs
+++ CatGacha/Models/Accounts/ChangePasswordViewModel.cs
@@ -1,6 +1,29 @@
-﻿namespace CatGacha.Models.Accounts
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CatGacha.Models.Accounts
 {
     public class ChangePasswordViewModel
     {
+        /* thought:
+         * in the future, changepassword and addpassword models could be combined into one password model.
+         * 
+         * as addpassword has two datapoints, changepassword function could be refactored in the frontend to use 2 instances of the 
+         * same password model. one for existing password one for new password.
+         * 
+         * i dont know how to do this yet
+         */
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Praegune Salasõna")]
+        public string CurrentPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Uus Salasõna")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Uue Salasõna Pimekoopia")]
+        public string ConfirmPassword { get; set; }
     }
 }
