a349fb7aadc40fd1d0ba5d8e99547a730d215578
Estlib <75094683+Estlib@users.noreply.github.com>
2024-09-13 13:05:00 +0300

    added iaccountsservices and accountdtos

---
+++ CatGacha.Core/Dto/AccountDtos/LoginDto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatGacha.Core.Dto.AccountDtos
+{
+    public class LoginDto
+    {
+        public string Email { get; set; }
+        public IList<AuthenticationScheme> ExternalLogins { get; set; }
+    }
+}
---
+++ CatGacha.Core/ServiceInterface/IAccountsServices.cs
@@ -0,0 +1,14 @@
+﻿using CatGacha.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatGacha.Core.ServiceInterface
+{
+    public interface IAccountsServices
+    {
+        Task<ApplicationUser> Register(ApplicationUserDto dto);
+    }
+}
