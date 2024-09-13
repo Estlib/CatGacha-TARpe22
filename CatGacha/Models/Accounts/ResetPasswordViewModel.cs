2989c95adacff811ad731261b46ff0648795702f
Estlib <75094683+Estlib@users.noreply.github.com>
2024-09-13 12:46:56 +0300

    added reset password model info and view

--- CatGacha/Models/Accounts/ResetPasswordViewModel.cs
+++ CatGacha/Models/Accounts/ResetPasswordViewModel.cs
@@ -1,6 +1,19 @@
-﻿namespace CatGacha.Models.Accounts
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CatGacha.Models.Accounts
 {
     public class ResetPasswordViewModel
     {
+        [Required]
+        [EmailAddress]
+        public string BackupEmail { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "Salasõnad ei ühti, palun kontrolli et kirjutasid mõlemad samamoodi.")]
+        public string ConfirmPassword { get; set; }
+        public string Token { get; set; }
     }
 }
